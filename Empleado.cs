87142f90e653c665f9070a220e387367a1f2d3bb
Nicolas Leal <nicoleal14h@gmail.com>
2022-10-13 23:49:00 -0300

    otro commit

--- Empleado.cs
+++ Empleado.cs
@@ -18,12 +18,12 @@ namespace tl2_tp2_2022_nico89h
     }
     internal class Empleado
     {
-        
-        
+
+        public static Logger logger=LogManager.GetCurrentClassLogger(); //con esta clase agarrare los errores
         
         //iincio de la clase empleado
         private int Dni;
-        private string direccion, nombre, apellido;
+        private string direccion=String.Empty, nombre = String.Empty, apellido = String.Empty;
         private DateTime fechaIngreso;
         private float sueldo;
         private Cargo cargo;
@@ -40,18 +40,30 @@ namespace tl2_tp2_2022_nico89h
             this.FechaIngreso = DateTime.Now;
             this.sueldo = 0;
             this.cargo = Cargo.Adminstrador;
+            logger.Warn("Advertencia, los datos cargados no tienen valor, es decir estan vacios");
+
         }
 
         //inicio de constructor sobreacrgado
         public Empleado(int dni, string direccion, string nombre, string apellido, DateTime fechaIngreso, float sueldo, Cargo cargo)
         {
-            this.Dni1 = dni;
-            this.Direccion = direccion;
-            this.Nombre = nombre;
-            this.Apellido = apellido;
-            this.FechaIngreso = fechaIngreso;
-            this.sueldo = sueldo;
-            this.cargo = cargo;
+            try
+            {
+                this.Dni1 = dni;
+                this.Direccion = direccion;
+                this.Nombre = nombre;
+                this.Apellido = apellido;
+                this.FechaIngreso = fechaIngreso;
+                this.sueldo = sueldo;
+                this.cargo = cargo;
+                logger.Trace("Los datos fueron cargados correctamente");
+            }
+            catch (Exception)
+            {
+
+                logger.Error("No se pudieron cargar los datos, revise nuevamente los mismos");
+            }
+            
         }
         //inicio de getter y setter
         public int Dni1 { get => Dni; set => Dni = value; }
@@ -88,13 +100,23 @@ namespace tl2_tp2_2022_nico89h
 
         public void mostrarDatos()
         {
-            Console.WriteLine("Los datos son: ");
-            Console.WriteLine("Dni: "+ this.Dni1);
-            Console.WriteLine("Apellido "+ this.Apellido);
-            Console.WriteLine("Nombre: "+ this.Nombre);
-            this.antiguedad();
-            Console.WriteLine("Antiguedad "+(DateTime.Now.Year - this.FechaIngreso.Year));
-            Console.WriteLine("Fecha de ingreso "+ this.FechaIngreso);
+            try
+            {
+                Console.WriteLine("Los datos son: ");
+                Console.WriteLine("Dni: " + this.Dni1);
+                Console.WriteLine("Apellido " + this.Apellido);
+                Console.WriteLine("Nombre: " + this.Nombre);
+                this.antiguedad();
+                Console.WriteLine("Antiguedad " + (DateTime.Now.Year - this.FechaIngreso.Year));
+                Console.WriteLine("Fecha de ingreso " + this.FechaIngreso);
+            }
+            catch (Exception ex)
+            {
+                
+                logger.Warn("La excepcion arrojada es: "+ ex.Message);
+                //throw;
+            }
+            
         }
 
     }
